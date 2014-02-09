d96ef50d9b79619997f4afda45c74c5096d385ef
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-02-09 16:47:16 +0800

    Add secret key

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'lTQ0PC1icCN3NQefU4qxneOF1VS3oqe6',
 
 	/*
 	|--------------------------------------------------------------------------
