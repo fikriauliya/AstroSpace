5a8f67a1eb6c764fa9a4862af98e3848245bc074
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-02-20 11:11:18 +0800

    Use bootstrap=typeaheahead in favour of twitter lookahead

--- app/views/blogposts/create.blade.php
+++ app/views/blogposts/create.blade.php
@@ -1,71 +1,9 @@
 @extends('layouts.master')
+
 @section('header')
-  <style>
-    .typeahead,
-    .tt-query,
-    .tt-hint {
-      width: 396px;
-      height: 30px;
-      padding: 8px 12px;
-      font-size: 24px;
-      line-height: 30px;
-      border: 2px solid #ccc;
-      -webkit-border-radius: 8px;
-         -moz-border-radius: 8px;
-              border-radius: 8px;
-      outline: none;
-    }
-
-    .typeahead {
-      background-color: #fff;
-    }
-
-    .typeahead:focus {
-      border: 2px solid #0097cf;
-    }
-
-    .tt-query {
-      -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
-         -moz-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
-              box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
-    }
-
-    .tt-hint {
-      color: #999
-    }
-
-    .tt-dropdown-menu {
-      width: 422px;
-      margin-top: 12px;
-      padding: 8px 0;
-      background-color: #fff;
-      border: 1px solid #ccc;
-      border: 1px solid rgba(0, 0, 0, 0.2);
-      -webkit-border-radius: 8px;
-         -moz-border-radius: 8px;
-              border-radius: 8px;
-      -webkit-box-shadow: 0 5px 10px rgba(0,0,0,.2);
-         -moz-box-shadow: 0 5px 10px rgba(0,0,0,.2);
-              box-shadow: 0 5px 10px rgba(0,0,0,.2);
-    }
-
-    .tt-suggestion {
-      padding: 3px 20px;
-      font-size: 18px;
-      line-height: 24px;
-    }
-
-    .tt-suggestion.tt-cursor {
-      color: #fff;
-      background-color: #0097cf;
-
-    }
-
-    .tt-suggestion p {
-      margin: 0;
-    }
-
-  </style>
+  {{ HTML::script('js/bloodhound.js')}}
+  {{ HTML::script('js/bootstrap3-typeahead.js')}}
+@stop
 
 @section('content')
   <div class="row">
@@ -109,41 +47,29 @@
         </div>
         <div class="form-group">
           <label for="visible_to">Visible to</label>
-          {{ Form::text('visible_to', null, array('dir'=>'auto', 'class'=>'typeahead tt-input', 'placeholder'=>'visible_to')) }}
+          {{ Form::text('visible_to', null, array('data-provide'=>"typeahead", 'autocomplete'=>'off', 'class'=>'form-control typeahead')) }}
         </div>
       </div>
     {{ Form::close() }}
   </div>
 
-
-  {{ HTML::script('js/typeahead.bundle.min.js')}}
-
   <script type="text/javascript">
     $(function() {
+      // instantiate the bloodhound suggestion engine
       var numbers = new Bloodhound({
-        datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.num); },
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
         queryTokenizer: Bloodhound.tokenizers.whitespace,
-        local: [
-          { num: 'one' },
-          { num: 'two' },
-          { num: 'three' },
-          { num: 'four' },
-          { num: 'five' },
-          { num: 'six' },
-          { num: 'seven' },
-          { num: 'eight' },
-          { num: 'nine' },
-          { num: 'ten' }
-          ]
-      });     
+        local:  ["(A)labama","Alaska","Arizona","Arkansas","Arkansas2","Barkansas"]
+      });
+
       // initialize the bloodhound suggestion engine
       numbers.initialize();
-       
-      // instantiate the typeahead UI
-      $('.typeahead').typeahead(null, {
-        displayKey: 'num',
-        source: numbers.ttAdapter()
-      });
+
+      $('.typeahead').typeahead(
+      {
+        items: 4,
+        source:numbers.ttAdapter()  
+      }); 
     });
   </script>
 @stop
