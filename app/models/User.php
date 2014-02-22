1fdbc581e58c1a88ed1355ffa0388e96d8307324
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-02-22 21:45:10 +0800

    Allow alphanumeric username

--- app/models/User.php
+++ app/models/User.php
@@ -5,7 +5,7 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 	public static $rules = array(
-    'username'=>'required|alpha|min:2',
+    'username'=>'required|alpha_num|min:2',
     'email'=>'required|email|unique:users',
     'password'=>'required|between:6,40|confirmed',
     'password_confirmation'=>'required|between:6,40'
