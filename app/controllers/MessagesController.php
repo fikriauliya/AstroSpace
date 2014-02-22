88a73ca1ee0ff7cf105306642f73e7bf23f9384e
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-02-22 17:07:16 +0800

    Fix the authorization for MessageController

--- app/controllers/MessagesController.php
+++ app/controllers/MessagesController.php
@@ -3,23 +3,8 @@
 class MessagesController extends BaseController {
 	public function __construct() {
     $this->beforeFilter('csrf', array('on'=>'post'));
-    $this->beforeFilter('auth', array('only'=>array('create', 'store', 'show')));
+    $this->beforeFilter('auth', array('only'=>array('create', 'store', 'index')));
 	}
-
-	// public function show($id) {
-	// 	$auth_user = Auth::user();
-	// 	$blog_post = BlogPost::find($id);
-	// 	if ($blog_post->posted_by_id != $auth_user->id && $blog_post->is_private) {
-	// 		$visible_tos = explode(',', $blog_post->visible_tos);
-
-	// 		if (!in_array($auth_user->id, $visible_tos)) {
-	// 			return Redirect::to('/')->with('message', 'This post is private');
-	// 		}
-	// 	}
-
-	// 	$comments = $blog_post->comments;
-	// 	return View::make('blogposts.show')->with('blogpost', $blog_post)->with('comments', $comments);
-	// }
 
 	public function create() {
 		$friends = Auth::user()->friends2;
