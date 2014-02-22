c53aea513a3557e464286087f39fd1c75afc9f1b
yosua_michael <yosua_michael@nus.edu.sg>
2014-02-23 03:57:30 +0800

    User more reliable ways to change comment_count

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -13,4 +13,24 @@ class Comment extends Eloquent {
   public function blogPost() {
     return $this->belongsTo('BlogPost', 'blog_post_id', 'id');
   }
+
+   public static function boot(){
+      parent::boot();
+
+      static::creating(function($comment){
+			$blogpost = BlogPost::find($comment->blog_post_id);
+			$blogpost->comment_count = $blogpost->comment_count+1;
+			$blogpost->save();
+		});
+
+		static::deleting(function($comment){
+			$blogpost = BlogPost::find($comment->blog_post_id);
+			$blogpost->comment_count = $blogpost->comment_count-1;
+			$blogpost->save();
+		});
+
+   }   
+
+
+
 }
