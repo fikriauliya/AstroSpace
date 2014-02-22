f2b3707cdc275ca31cafcd729d21827c10967c8b
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-02-22 19:28:24 +0800

    Refactor comment authorization checking

--- app/controllers/CommentsController.php
+++ app/controllers/CommentsController.php
@@ -3,19 +3,15 @@
 class CommentsController extends BaseController {
   public function __construct() {
     $this->beforeFilter('csrf', array('on'=>'post'));
-    $this->beforeFilter('auth', array('only'=>array('create', 'store')));
+    $this->beforeFilter('auth', array('only'=>array('store')));
   }
 
   public function store() {
     $auth_user_id = Auth::user()->id;
 
     $blog_post = BlogPost::find(Input::get('blog_post_id'));
-    if ($blog_post->posted_by_id != $auth_user_id && $blog_post->is_private) {
-      $visible_tos = explode(',', $blog_post->visible_tos);
-
-      if (!in_array($auth_user_id, $visible_tos)) {
-        return Redirect::to('/')->with('message', 'This post is private');
-      }
+    if (!$blog_post->is_visible_to_user()) {
+      return Redirect::to('/')->with('message', 'This post is private');
     }
     $comment = new Comment;
     $comment->content = Input::get('content');
