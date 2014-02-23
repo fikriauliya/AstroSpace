f7e9aae191d698de12cd357994e13b80b085d8d3
Melvin <kfyong@nus.edu.sg>
2014-02-23 12:41:23 +0800

    Minor updates on show.blade.php

--- app/views/statistics/show.blade.php
+++ app/views/statistics/show.blade.php
@@ -3,26 +3,29 @@
 @section('content')
 <div class="row">
 	<div class="container-fluid" id="stats_1">
-		<h3>Statistic1 title</h3>
+		<h3>Statistics1: Most commented post</h3>
 		<!-- Put the statistic here -->
-
+		$blogpost = $user->blogposts;
+		foreach($blogposts as $blogpost){
+			echo $blogspot->title,"",$blogspot->comment_count;
+		}
 
 	</div><!-- stats_1 -->
 
 	<div class="container-fluid" id="stats_2">
-		<h3>Statistic2 title</h3>
+		<h3>Statistics2: Number of incoming emails</h3>
 		<!-- Put the statistic here -->
 
 	</div><!-- stats_2 -->
 
 	<div class="container-fluid" id="stats_3">
-		<h3>Statistic3 title</h3>
+		<h3>Statistics3: Visitor's country</h3>
 		<!-- Put the statistic here -->
 
 	</div><!-- stats_3 -->
 
 	<div class="container-fluid" id="stats_4">
-		<h3>Statistic4 title</h3>
+		<h3>Statistics4: Number of visits</h3>
 		<!-- Put the statistic here -->
 
 	</div><!-- stats_4 -->
