e2a5b6e07b8f98d76b217a1d4a6b8c72a35bacab
Melvin <kfyong@nus.edu.sg>
2014-02-23 13:31:20 +0800

    Statistics update

--- app/views/statistics/show.blade.php
+++ app/views/statistics/show.blade.php
@@ -3,29 +3,32 @@
 @section('content')
 <div class="row">
 	<div class="container-fluid" id="stats_1">
-		<h3>Statistics1: Most commented post</h3>
+		<h3>Statistics 1: Most commented post</h3>
 		<!-- Put the statistic here -->
-		$blogpost = $user->blogposts;
+		<?php
+		$blogposts = $user->blogposts;
 		foreach($blogposts as $blogpost){
-			echo $blogspot->title,"",$blogspot->comment_count;
-		}
+			echo "The number of comments of post '".$blogpost->title."', "." number of comments: ".$blogpost->comment_count;
+		}?>
 
 	</div><!-- stats_1 -->
 
 	<div class="container-fluid" id="stats_2">
-		<h3>Statistics2: Number of incoming emails</h3>
+		<h3>Statistics 2: Number of incoming emails</h3>
 		<!-- Put the statistic here -->
-
+		<?php
+			echo "number of user's email: ".Message::where('recipient_id', '=', $user->id)->count();
+		?>
 	</div><!-- stats_2 -->
 
 	<div class="container-fluid" id="stats_3">
-		<h3>Statistics3: Visitor's country</h3>
+		<h3>Statistics 3: Visitor's country</h3>
 		<!-- Put the statistic here -->
 
 	</div><!-- stats_3 -->
 
 	<div class="container-fluid" id="stats_4">
-		<h3>Statistics4: Number of visits</h3>
+		<h3>Statistics 4: Number of visits</h3>
 		<!-- Put the statistic here -->
 
 	</div><!-- stats_4 -->
